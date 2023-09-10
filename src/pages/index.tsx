d89c442b1736f1e51ffb7c2a7f5737460d3d7b18
jen-frontend <jen.frontend@gmail.com>
2023-09-10 14:46:20 +0900

    feat: 카카오 지도 (kakao map API)불러오기 & 지도 컴포넌트 실습

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,5 @@
-import Link from "next/link";
+import Map from "@/components/Map";
 
 export default function Home() {
-  return (
-    <>
-      <h1 className="font-bold text-lg">Map Index Page</h1>
-      <ul className="px-4 py-2">
-        <li>
-          <Link href="/stores">맛집 목록</Link>
-        </li>
-        <li>
-          <Link href="/stores/new">맛집 생성</Link>
-        </li>
-        <li>
-          <Link href="/stores/1">맛집 상세 페이지</Link>
-        </li>
-        <li>
-          <Link href="/stores/1/edit">맛집 수정 페이지</Link>
-        </li>
-        <li>
-          <Link href="/users/login">로그인 페이지</Link>
-        </li>
-        <li>
-          <Link href="/users/mypage">마이페이지</Link>
-        </li>
-        <li>
-          <Link href="/users/likes">찜한 맛집</Link>
-        </li>
-      </ul>
-    </>
-  );
+  return <Map />;
 }
