2bf7307d6df446333f010af8c6dca1111ddd4f48
jen-frontend <jen.frontend@gmail.com>
2023-09-10 20:46:18 +0900

    feat: prisma 데이터 seed 실습

---
+++ prisma/migrations/20230910111647_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Store" (
+    "id" SERIAL NOT NULL,
+    "phone" TEXT,
+    "address" TEXT,
+    "lat" TEXT,
+    "lng" TEXT,
+    "name" TEXT,
+    "category" TEXT,
+    "storeType" TEXT,
+    "foodCertifyName" TEXT,
+
+    CONSTRAINT "Store_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
