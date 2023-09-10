8654ff0927aa14e493bb0a3f8160a3785492bd7e
jen-frontend <jen.frontend@gmail.com>
2023-09-10 14:18:48 +0900

    feat: kakao developers 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import Layout from "@/component/Layout";
+import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1 className="font-bold">Map Index Page</h1>
+      <h1 className="font-bold text-lg">Map Index Page</h1>
       <ul className="px-4 py-2">
         <li>
           <Link href="/stores">맛집 목록</Link>
