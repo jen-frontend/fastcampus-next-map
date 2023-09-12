3c8a89345e6a7c4d14f84c07463a4bdb00994c47
jen-frontend <jen.frontend@gmail.com>
2023-09-12 17:09:10 +0900

    feat: React Query 로 데이터 가져오기

--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -7,7 +7,9 @@ export default async function handler(
   res: NextApiResponse<StoreType[]>
 ) {
   const prisma = new PrismaClient();
-  const stores = await prisma.store.findMany();
+  const stores = await prisma.store.findMany({
+    orderBy: { id: "asc" },
+  });
 
   res.status(200).json(stores);
 }
--- src/pages/stores/index.tsx
+++ src/pages/stores/index.tsx
@@ -1,54 +1,70 @@
 import Image from "next/image";
 import { StoreType } from "@/interface";
 
-import axios from "axios";
+import { useQuery } from "react-query";
 
-export default function StoreListPage({ stores }: { stores: StoreType[] }) {
+import axios from "axios";
+import Loading from "@/components/Loading";
+
+export default function StoreListPage() {
+  const {
+    isLoading,
+    isError,
+    data: stores,
+  } = useQuery("stores", async () => {
+    const { data } = await axios("/api/stores");
+    return data as StoreType[];
+  });
+
+  if (isError) {
+    return (
+      <div className="w-full h-screen mx-auto pt-[10%] text-red-500 text-center font-semibold">
+        다시 시도해주세요
+      </div>
+    );
+  }
+
   return (
     <div className="px-4 md:max-w-4xl mx-auto py-8">
       <ul role="list" className="divide-y divide-gray-100">
-        {stores?.map((store, index) => (
-          <li className="flex justify-between gap-x-6 py-5" key={index}>
-            <div className="flex gap-x-4">
-              <Image
-                src={
-                  store?.category
-                    ? `/images/markers/${store?.category}.png`
-                    : "/images/markers/default.png"
-                }
-                width={48}
-                height={48}
-                alt="아이콘 이미지"
-              />
-              <div>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          stores?.map((store, index) => (
+            <li className="flex justify-between gap-x-6 py-5" key={index}>
+              <div className="flex gap-x-4">
+                <Image
+                  src={
+                    store?.category
+                      ? `/images/markers/${store?.category}.png`
+                      : "/images/markers/default.png"
+                  }
+                  width={48}
+                  height={48}
+                  alt="아이콘 이미지"
+                />
+                <div>
+                  <div className="text-sm font-semibold leading-6 text-gray-900">
+                    {store?.name}
+                  </div>
+                  <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
+                    {store?.storeType}
+                  </div>
+                </div>
+              </div>
+              <div className="hidden sm:flex sm:flex-col sm:items-end">
                 <div className="text-sm font-semibold leading-6 text-gray-900">
-                  {store?.name}
+                  {store?.address}
                 </div>
                 <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
-                  {store?.storeType}
+                  {store?.phone || "번호없음"} | {store?.foodCertifyName} |{" "}
+                  {store?.category}
                 </div>
               </div>
-            </div>
-            <div className="hidden sm:flex sm:flex-col sm:items-end">
-              <div className="text-sm font-semibold leading-6 text-gray-900">
-                {store?.address}
-              </div>
-              <div className="mt-1 text-xs truncate font-semibold leading-5 text-gray-500">
-                {store?.phone || "번호없음"} | {store?.foodCertifyName} |{" "}
-                {store?.category}
-              </div>
-            </div>
-          </li>
-        ))}
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`);
-
-  return {
-    props: { stores: stores.data },
-  };
 }
