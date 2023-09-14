f5fff1389cab4eb705832d5a87ab8fc0a73df585
jen-frontend <jen.frontend@gmail.com>
2023-09-14 17:17:34 +0900

    feat: 무한 스크롤 구현하기

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+export default function Loader() {
+  return (
+    <div className="flex gap-4 justify-center mt-10">
+      <div className="w-2 h-2 animate-ping rounded-full bg-gray-500" />
+      <div className="w-2 h-2 animate-ping rounded-full bg-gray-500" />
+      <div className="w-2 h-2 animate-ping rounded-full bg-gray-500" />
+    </div>
+  );
+}
