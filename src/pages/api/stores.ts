9fee649cb0de1f41930e3aa1e5f2494cefab9aa1
jen-frontend <jen.frontend@gmail.com>
2023-09-16 00:17:52 +0900

    feat: 검색 & 정렬 구현하기

--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -2,11 +2,18 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { StoreApiResponse, StoreType } from "@/interface";
 import { PrismaClient } from "@prisma/client";
 
+interface Responsetype {
+  page?: string;
+  limit?: string;
+  q?: string;
+  district?: string;
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<StoreApiResponse | StoreType[] | StoreType>
 ) {
-  const { page = "" }: { page?: string } = req.query;
+  const { page = "", limit = "", q, district }: Responsetype = req.query;
   const prisma = new PrismaClient();
 
   if (page) {
@@ -14,7 +21,11 @@ export default async function handler(
     const skipPage = parseInt(page) - 1;
     const stores = await prisma.store.findMany({
       orderBy: { id: "asc" },
-      take: 10,
+      where: {
+        name: q ? { contains: q } : {},
+        address: district ? { contains: district } : {},
+      },
+      take: parseInt(limit),
       skip: skipPage * 10,
     });
 
