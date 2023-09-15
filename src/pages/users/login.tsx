4d4082ec41ef6dfb43a46c32bb06a7197273a828
jen-frontend <jen.frontend@gmail.com>
2023-09-15 14:52:19 +0900

    feat: Naver 로그인 화면 수정

--- src/pages/users/login.tsx
+++ src/pages/users/login.tsx
@@ -1,12 +1,11 @@
 import { useEffect } from "react";
 
-import Layout from "@/components/Layout";
-import { signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-
 import { AiOutlineGoogle } from "react-icons/ai";
 import { SiNaver } from "react-icons/si";
 import { RiKakaoTalkFill } from "react-icons/ri";
+
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function LoginPage() {
   const { status, data: session } = useSession();
@@ -19,54 +18,45 @@ export default function LoginPage() {
   }, [router, status]);
 
   return (
-    <Layout>
-      <div className="flex h-[60vh] flex-col justify-center px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <div className="text-blue-800 text-center text-2xl font-semibold italic">
-            eatmap
-          </div>
-          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-600">
-            SNS 계정으로 로그인해주세요
-          </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            계정이 없다면 자동으로 회원가입이 진행됩니다.
-          </p>
+    <div className="flex flex-col justify-center px-6 lg:px-8 h-[60vh]">
+      <div className="mx-auto w-full max-w-sm">
+        <div className="text-blue-800 text-center text-2xl font-semibold italic">
+          Nextmap
         </div>
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <div className="flex flex-col gap-2">
-            <button
-              type="button"
-              onClick={() => signIn("google")}
-              className="text-white relative group flex bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:ring-[#4285F4]/50 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
-            >
-              <span className="absolute inset-y-0 left-0 flex items-center pl-4">
-                <AiOutlineGoogle className="w-6 h-6" />
-              </span>
-              Sign in with Google
-            </button>
-            <button
-              type="button"
-              onClick={() => signIn("naver")}
-              className="text-white relative group flex bg-[#2db400] hover:bg-[#2db400]/90 focus:ring-4 focus:ring-[#2db400]/50 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center dark:focus:ring-[#2db400]/55 mr-2 mb-2"
-            >
-              <span className="absolute inset-y-0 left-0 flex items-center pl-4">
-                <SiNaver className="w-4 h-4 ml-1" />
-              </span>
-              Sign in with Naver
-            </button>
-            <button
-              type="button"
-              onClick={() => signIn("kakao")}
-              className="text-black relative group flex bg-[#fef01b] hover:bg-[#fef01b]/90 focus:ring-4 focus:ring-[#fef01b]/50 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center dark:focus:ring-[#fef01b]/55 mr-2 mb-2"
-            >
-              <span className="absolute inset-y-0 left-0 flex items-center pl-4">
-                <RiKakaoTalkFill className="w-6 h-6" />
-              </span>
-              Sign in with Kakao
-            </button>
-          </div>
+        <div className="text-center mt-6 text-2xl font-bold text-gray-600">
+          SNS 계정으로 로그인해주세요
+        </div>
+        <p className="mt-2 text-center text-sm text-gray-600">
+          계정이 없다면 자동으로 회원가입이 진행됩니다.
+        </p>
+      </div>
+      <div className="mt-10 mx-auto w-full max-w-sm">
+        <div className="flex flex-col gap-3">
+          <button
+            type="button"
+            onClick={() => signIn("google")}
+            className="text-white flex gap-2 bg-[#4285F4] hover:bg-[#4285F4]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
+          >
+            <AiOutlineGoogle className="w-6 h-6" />
+            Sign in with Google
+          </button>
+          <button
+            type="button"
+            onClick={() => signIn("naver")}
+            className="text-white flex gap-3 bg-[#2db400] hover:bg-[#2db400]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
+          >
+            <SiNaver className="w-4 h-4" />
+            Sign in with Naver
+          </button>
+          <button
+            type="button"
+            className="text-black flex gap-2 bg-[#fef01b] hover:bg-[#fef01b]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
+          >
+            <RiKakaoTalkFill className="w-6 h-6" />
+            Sign in with Kakao
+          </button>
         </div>
       </div>
-    </Layout>
+    </div>
   );
 }
