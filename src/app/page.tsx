c0d5def7be8da39711fddd160e3ba4003c3520c2
jen-frontend <jen.frontend@gmail.com>
2023-09-22 19:59:17 +0900

    feat: fetch API 예외처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,13 +19,17 @@ export default async function Home() {
 }
 
 async function getData() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`, {
-    cache: "no-store",
-  });
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`, {
+      cache: "no-store",
+    });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (e) {
+    console.log(e);
   }
-
-  return res.json();
 }
